fbef0c6f988f7231bd44545bfa2ef8f6d98b8951
Ezra Thomas Hampton III <ezrathomashamptoniii@Quinyattas-MacBook-Pro.local>
2023-03-18 09:30:20 -0400

    adding on to project basics

---
+++ Variable.cs
@@ -0,0 +1,15 @@
+using System
+
+namespace Variables
+{
+    class Program{
+        static void Main(){
+            string name = "Ezra";
+            int num = 3;
+            bool student = true;
+            float fp = 3.5;
+            double fp2 = 34.2;
+        
+        }
+    }
+}
