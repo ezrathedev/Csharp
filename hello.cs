ef2823145c6a9a2f9d824ed93e5085384955462e
Ezra Thomas Hampton III <ezrathomashamptoniii@Quinyattas-MacBook-Pro.local>
2023-03-18 08:50:46 -0400

    Working on csharp basics

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+        // using Console.WriteLine() i can post something to the console.
+      Console.WriteLine("Hello Ezra!");    
+     }
+  }
+}
